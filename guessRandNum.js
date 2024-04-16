c5829cb53ad5930398dce0d47d0fa70894877718
TajahH <167078781+TajahH@users.noreply.github.com>
2024-04-16 15:29:19 +0100

    Update guessRandNum.js

--- guessRandNum.js
+++ guessRandNum.js
@@ -3,7 +3,7 @@ const readline = require("readline-sync");
 let randNum = Math.floor(Math.random()*100+1) 
 let guesses = 0;
 let userNum = "";
-console.log(randNum)
+//console.log(randNum)  this line can be used for debugging purposes
 do {
 
     userNum = readline.question("Guess a number between 1 and 100, and see if it matches the computer: ");
